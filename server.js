57f667b73ef3ccabf1d23b88c50fd3086b6fab3e
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-04-08 12:57:48 -0700

    add server functionality

--- server.js
+++ server.js
@@ -7,11 +7,50 @@ const PORT = process.env.PORT || 3000
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-// Routes
+const waitlist = []
+const tables = []
+
+/**
+ * @route /
+ * @method get
+ * @return home.html
+ */
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "view.html"))
+  res.sendFile(path.join(__dirname, "home.html"))
 })
 
+app.get("/tables", (req, res) => {
+  res.sendFile(path.join(__dirname, "tables.html"))
+})
+
+app.get("/reserve", (req, res) => {
+  res.sendFile(path.join(__dirname, "reserve.html"))
+})
+
+app.get("/api/tables", (req, res) => {
+  res.json(tables)
+})
+
+app.get("/api/waitlist", (req, res) => {
+  res.json(waitlist)
+})
+
+app.post("/api/tables", (req, res) => {
+  const reservation = req.body
+  let resp = false
+
+  if (tables.length < 5) {
+    tables.push(reservation)
+    resp = true
+  } else {
+    waitlist.push(reservation)
+    resp = false
+  }
+
+  res.send(resp)
+})
+
+// Start Server
 app.listen(PORT, () => {
   console.log("App listening on PORT " + PORT)
 })
